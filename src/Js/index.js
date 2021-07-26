1ded4a73f5bd4132429dacbeccc0e3a4994ae443
Andrey Bulanov <bulanov1804@gmail.com>
2021-07-26 22:52:11 +0300

    check

--- src/Js/index.js
+++ src/Js/index.js
@@ -1,14 +1,64 @@
-import './sass/main.scss';
+import '../sass/main.scss';
 
-// import servisApi from '../Js/servisApi';
-// import refs from '../Js/refs';
-// import galleryMarkup from '../templates/gallery-item.hbs';
-// import * as basicLightbox from 'basiclightbox';
-// import '../../node_modules/'
+import servisApi from '../Js/servisApi';
+import refs from '../Js/refs';
+import galleryMarkup from '../templates/gallery-item.hbs';
+import * as basicLightbox from 'basiclightbox';
+import '../../node_modules/basicLightbox/dist/basicLightbox.min.css'
 
 const { form, gallery, loadMoreBtn, loadMoreLabel, loadMoreSpinner } = refs;
 
+form.addEventListener('submit', e => {
+  e.preventDefault();
 
-import fetchImg from './servisApi';
-fetchImg().then(data => console.log(data)
-)
+  const input = e.currentTarget.elements.query.value;
+  servisApi.query = input.trim();
+
+  if (!servisApi.query) return;
+  clearImgGallery();
+
+  servisApi.resetPage();
+
+  fetchMoreImg();
+
+  gallery.addEventListener('click', e => {
+    e.preventDefault();
+
+    if (e.target.nodeName === 'IMG') {
+        const instance = basicLightbox.create(
+            `<img src="${e.target.dataset.source}">`);
+      instance.show();
+    }
+  });
+
+  form.reset();
+});
+
+loadMoreBtn.addEventListener('click', fetchMoreImg);
+
+function fetchMoreImg() {
+  loadMore.disable();
+
+  servisApi.fetchImg().then(data => {
+    renderImageGallery(data);
+    loadMore.show();
+    loadMore.enable();
+
+    // const element = document.getElementById('.my-element-selector');
+    loadMoreBtn.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  });
+
+  // const element = document.getElementById('.my-element-selector');
+}
+
+function renderImageGallery(data) {
+  const markup = galleryMarkup(data);
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearImgGallery() {
+  gallery.innerHTML = '';
+}
--- src/Js/servisApi.js
+++ src/Js/servisApi.js
@@ -4,6 +4,34 @@ axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const API_KEY = '22635337-a2d7cfd18b30a4b0e9b9bd466';
 
-export default function fetchImg() {
-    return axios.get(`/?image_type=photo&orientation=horizontal&q=car&page=1&per_page=12&key=${API_KEY}`);
-}
+export default {
+    searchQuery: '',
+    page: 1,
+    
+    fetchImg() {
+        return axios.get(`/?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${API_KEY}`,)
+            .then(({ data }) => {
+                this.incrementPage();
+                return data.hits;
+            })
+            .catch(error => console.log(error));
+    },
+
+    incrementPage() {
+        this.page += 1;
+    },
+
+    resetPage() {
+        this.page = 1;
+    },
+
+    get query() {
+        return this.searchQuery;
+    },
+
+    set query(value) {
+        this.searchQuery = value;
+    },
+};
+
+
