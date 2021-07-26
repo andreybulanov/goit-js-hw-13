c16c94400296c99c27887c0d7659406ab30250cd
Andrey Bulanov <bulanov1804@gmail.com>
2021-07-26 19:55:56 +0300

    Late

--- src/Js/index.js
+++ src/Js/index.js
@@ -1,11 +1,14 @@
 import './sass/main.scss';
 
-import servisApi from '../Js/servisApi';
-import refs from '../Js/refs';
-import galleryMarkup from '../templates/gallery-item.hbs';
+// import servisApi from '../Js/servisApi';
+// import refs from '../Js/refs';
+// import galleryMarkup from '../templates/gallery-item.hbs';
 // import * as basicLightbox from 'basiclightbox';
 // import '../../node_modules/'
 
 const { form, gallery, loadMoreBtn, loadMoreLabel, loadMoreSpinner } = refs;
 
 
+import fetchImg from '../Js/servisApi';
+fetchImg().then(data => console.log(data)
+)
--- src/Js/servisApi.js
+++ src/Js/servisApi.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const API_KEY = '22635337-a2d7cfd18b30a4b0e9b9bd466';
+
+export default function fetchImg() {
+    return axios.get(`/?image_type=photo&orientation=horizontal&q=car&page=1&per_page=12&key=${API_KEY}`);
+}
