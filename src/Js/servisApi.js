8d10fb7903fe359c75949d355acd3666d9bee0b0
Andrey Bulanov <bulanov1804@gmail.com>
2021-07-29 20:27:10 +0300

    add Notiflix

--- src/Js/servisApi.js
+++ src/Js/servisApi.js
@@ -5,33 +5,36 @@ axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '22635337-a2d7cfd18b30a4b0e9b9bd466';
 
 export default {
-    searchQuery: '',
-    page: 1,
-    
-    fetchImg() {
-        return axios.get(`/?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${API_KEY}`,)
-            .then(({ data }) => {
-                this.incrementPage();
-                return data.hits;
-            })
-            .catch(error => console.log(error));
-    },
+  searchQuery: '',
+  page: 1,
 
-    incrementPage() {
-        this.page += 1;
-    },
+  fetchImg() {
+    return axios
+      .get(
+        `/?image_type=photo&orientation=horizontal&safesearch=true&q=${this.query}&page=${this.page}&per_page=40&key=${API_KEY}`,
+      )
+      .then(({ data }) => {
+        this.incrementPage();
+        return data.hits;
+      })
+      .catch(error => console.error(error));
+  },
 
-    resetPage() {
-        this.page = 1;
-    },
+  incrementPage() {
+    this.page += 1;
+  },
 
-    get query() {
-        return this.searchQuery;
-    },
+  resetPage() {
+    this.page = 1;
+  },
 
-    set query(value) {
-        this.searchQuery = value;
-    },
+  get query() {
+    return this.searchQuery;
+  },
+
+  set query(value) {
+    this.searchQuery = value;
+  },
 };
 
 
